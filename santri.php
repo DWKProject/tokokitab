0c3ff3929287b7f0256302e93d8674b8582d722b
Ronal62 <ichojuraku@gmail.com>
2022-08-07 09:49:51 +0700

    Menambah Santri.php

--- santri.php
+++ santri.php
@@ -1,18 +1,18 @@
 <?php include 'header.php'; ?>
 <div class="page-heading">
-    <h1 class="page-title">Data Kitab</h1>
+    <h1 class="page-title">Data Santri</h1>
     <ol class="breadcrumb">
         <li class="breadcrumb-item">
             <a href="index.html"><i class="la la-home font-20"></i></a>
         </li>
-        <li class="breadcrumb-item">Data Kitab</li>
+        <li class="breadcrumb-item">Data Santri</li>
     </ol>
 </div>
 <div class="page-content fade-in-up">
     <div class="ibox">
         <div class="ibox-head">
-            <div class="ibox-title">Data Kitab</div>
-            <a href="tambah_kitab.php" class="btn btn-primary  btn-rounded pull-right"><i class="fa ti-plus"> Tambah kitab</i></a>
+            <div class="ibox-title">Data Santi</div>
+            <a href="tambah_kitab.php" class="btn btn-primary  btn-rounded pull-right"><i class="fa ti-plus"> Tambah Santri</i></a>
         </div>
         <div class="ibox-body">
             <br>
@@ -20,12 +20,11 @@
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>Kode Kitab</th>
+                        <th>Nis</th>
                         <th>Nama</th>
-                        <th>Stok</th>
-                        <th>Harga Kolak</th>
-                        <th>Harga Jual</th>
-                        <th>Gambar</th>
+                        <th>Alamat</th>
+                        <th>Kelas</th>
+                        <th>Kamar</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -34,20 +33,19 @@
                     <?php
                     $no = 1;
                     include 'koneksi.php';
-                    $sql = mysqli_query($conn, "SELECT * FROM kitab");
+                    $sql = mysqli_query($conn, "SELECT * FROM tb_santri");
                     while ($data = mysqli_fetch_assoc($sql)) {
                     ?>
                         <tr>
                             <td><?= $no++; ?></td>
-                            <td><?= $data['kd_kitab']; ?></td>
+                            <td><?= $data['nis']; ?></td>
                             <td><?= $data['nama']; ?></td>
-                            <td><?= $data['stok']; ?></td>
-                            <td><?= rupiah($data['harga_kolak']); ?></td>
-                            <td><?= rupiah($data['harga_jual']); ?></td>
-                            <td><?= $data['gambar']; ?></td>
+                            <td><?= $data['desa']; ?> - <?= $data['kec']; ?></td>
+                            <td><?= $data['k_formal']; ?> - <?= $data['t_formal']; ?></td>
+                            <td><?= $data['kamar']; ?> - <?= $data['komplek']; ?></td>
                             <td>
-                                <a href="edit_kitab.php?id=<?= $data['id_kitab']; ?>" class="btn btn-warning "><i class="fa fa-pencil"></i></a>
-                                <a href="<?= 'hapus_kitab.php?id_kitab=' . $data['id_kitab'] ?>" onclick="return confirm('Yakin Menghapus Data Ini?')" class="btn btn-danger "><i class="fa fa-times"></i></a>
+                                <!-- <a href="edit_kitab.php?id=<?= $data['id_kitab']; ?>" class="btn btn-warning "><i class="fa fa-pencil"></i></a>
+                                <a href="<?= 'hapus_kitab.php?id_kitab=' . $data['id_kitab'] ?>" onclick="return confirm('Yakin Menghapus Data Ini?')" class="btn btn-danger "><i class="fa fa-times"></i></a> -->
                             </td>
                         </tr>
                     <?php } ?>
