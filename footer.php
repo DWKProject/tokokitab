20025ba73657c451ba88bd1272993d475867b61d
Ronal62 <ichojuraku@gmail.com>
2022-08-12 23:06:11 +0700

    push

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="page-footer">
-    <div class="font-13">2018 © <b>AdminCAST</b> - All rights reserved.</div>
-    <a class="px-4" href="http://themeforest.net/item/adminca-responsive-bootstrap-4-3-angular-4-admin-dashboard-template/20912589" target="_blank">BUY PREMIUM</a>
+    <div class="font-13">2022 © <b>SMKDWK</b> - All rights reserved.</div>
+    <a class="px-4" href="https://www.youtube.com/channel/UCS0Niu7LpNR_NtsDkEqvAfA" target="_blank">SUBSCRIBE YT</a>
     <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
 </footer>
 </div>
